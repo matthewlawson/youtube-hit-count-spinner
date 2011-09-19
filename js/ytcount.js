88f9a0f8274725d35caf8f34bc67bb5248ffc61c
Matthew Lawson <mattlaw1@gmail.com>
2011-09-19 22:15:11 +0100

    Run only on youtube.com, toggle start stop

--- js/ytcount.js
+++ js/ytcount.js
@@ -1,10 +1,45 @@
-var countElement = document.getElementById("watch-actions-right").getElementsByTagName('strong')[0].childNodes[0];
+if(document.getElementById("watch-actions-right") && document.getElementById("watch-actions-right").getElementsByTagName('strong') && document.getElementById("watch-actions-right").getElementsByTagName('strong')[0].childNodes[0])
+{
+    var countElement = document.getElementById("watch-actions-right").getElementsByTagName('strong')[0].childNodes[0];
 
-var ytCount  = countElement.textContent;
+}
+if(!countElement)
+{
+    alert('The youtube spinner cannot be ran on this page.');
+}
+else
+{
+    if(typeof intervalId != "undefined")
+    {
+        console.log(intervalId);
+        window.clearInterval(intervalId);
+        intervalId = undefined;
+        //alert('its running');
+    }
+    else
+    {
 
-ytCount = ytCount.replace(",", "")
-//console.log(ytCount);
-setInterval(function(){
-    ytCount ++;
-    countElement.textContent = ytCount;
-}, 100);
+        var ytCount  = countElement.textContent;
+
+        ytCount = ytCount.replace(/,/gi, "");
+
+    //    console.log(ytCount);
+        var intervalId = window.setInterval(function(){
+            ytCount++;
+            //console.log(ytCount);
+            fmtCount = ytCount;
+            var rgx = /(\d+)(\d{3})/;
+
+            while(rgx.test(fmtCount)) {
+                    //fmtCount = fmtCount+ '';
+                    fmtCount = fmtCount.toString().replace(rgx, '$1' + ',' + '$2');
+                    //console.log(fmtCount);
+            }
+            //console.log(ytCount);
+            countElement.textContent = fmtCount;
+        }, 150);
+    }
+
+
+}
+
